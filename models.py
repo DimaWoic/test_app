ad6a0d6b6b285cfce0a0b57839ade9923c40d8a8
DimaWoic <woicdv@gmail.com>
2020-07-28 00:07:12 +0400

    добавлена модель Articles, серилизатор, сконфигурированы urls, добавлен во view пагинация и viewset, рабочая админка

--- models.py
+++ models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=250, unique=True, verbose_name='заголовок статьи')
+    text = models.TextField(verbose_name='текст статьи')
+    source = models.URLField(verbose_name='ссылка на оригинал')
+    published = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
+        ordering = ['-published']
+
+    def __str__(self):
+        return self.title
--- views.py
+++ views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
-# Create your views here.
+
+class ArticlePagination(PageNumberPagination):
+    page_size = 30
+    page_size_query_param = 'page_size'
+    max_page_size = 200
+    last_page_strings = ('the_end',)
+
+
+class ArticleView(ReadOnlyModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    pagination_class = ArticlePagination
