f72e0c4ffba9ba2d3d5195540a89f6820b4131ba
DimaWoic <woicdv@gmail.com>
2020-08-02 16:44:03 +0400

    Дополнен README.md, добавлена функция documentation() во views, выводящая описание всех классов содержащихся во views

--- views.py
+++ views.py
@@ -3,15 +3,28 @@ from .models import Article
 from .serializers import ArticleListSerializer, ArticleSerializer
 from rest_framework.pagination import PageNumberPagination
 
-"""class ArticlePagination - предназнчен для реализации пагинации в списка статей в API
-                                 наследуется от rest_framework.pagination.PageNumberPagination, имеет следующие атрибуты
-                                 page_size - количество выводимых данных поумолчанию на одной странице
-                                 page_size_query_param - параметр для передачи в http-запросе о смене количества
-                                                         выводимых записей на одной странице
-                                 max_page_size - максимально возможное количество записей для одной страницы
-                                 last_page_strings - используется для запроса последней страницы
 
-"""
+def documentation():
+
+        """class ArticlePagination - предназнчен для реализации пагинации в списка статей в API
+                                         наследуется от rest_framework.pagination.PageNumberPagination, имеет следующие атрибуты
+                                         page_size - количество выводимых данных поумолчанию на одной странице
+                                         page_size_query_param - параметр для передачи в http-запросе о смене количества
+                                                                 выводимых записей на одной странице
+                                         max_page_size - максимально возможное количество записей для одной страницы
+                                         last_page_strings - используется для запроса последней страницы
+           class ArticleView - выодит листинг статей и обрабатывает метод GET, наследуется от
+                                   rest_framework.generics.ListAPIView и имеет следующие атрибуты:
+                                   queryset - атрибут указывающий диспетчер записи, где будет осуществляться поиск записи
+                                   serializer_class - атрибут указывающий сериализатор
+                                   pagination_class - атрибут указывающий переопределённый класс пагинатора
+            class DetailView - выодит отдельную статью и обрабатывает метод GET, наследуется от
+                                       rest_framework.generics.RetrieveAPIView и имеет следующие атрибуты:
+                                       queryset - атрибут указывающий диспетчер записи, где будет осуществляться поиск записи
+                                       serializer_class - атрибут указывающий сериализатор
+
+        """
+        pass
 
 
 class ArticlePagination(PageNumberPagination):
@@ -22,26 +35,11 @@ class ArticlePagination(PageNumberPagination):
     last_page_strings = ('end',)
 
 
-"""class ArticleView - выодит листинг статей и обрабатывает метод GET, наследуется от
-                           rest_framework.generics.ListAPIView и имеет следующие атрибуты:
-                           queryset - атрибут указывающий диспетчер записи, где будет осуществляться поиск записи
-                           serializer_class - атрибут указывающий сериализатор
-                           pagination_class - атрибут указывающий переопределённый класс пагинатора
-"""
-
-
 class ArticleView(ListAPIView):
 
     queryset = Article.objects.all()
     serializer_class = ArticleListSerializer
     pagination_class = ArticlePagination
-
-
-"""class DetailView - выодит отдельную статью и обрабатывает метод GET, наследуется от
-                               rest_framework.generics.RetrieveAPIView и имеет следующие атрибуты:
-                               queryset - атрибут указывающий диспетчер записи, где будет осуществляться поиск записи
-                               serializer_class - атрибут указывающий сериализатор
-"""
 
 
 class DetailView(RetrieveAPIView):
